51fbffbbcadf13bb965c656eadd30196b90bd305
elena-vystavnaya <helenv2909@gmail.com>
2018-07-25 23:33:44 +0300

    async css

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    $("head").append("<link rel='stylesheet' type='text/css' href='css/style.min.css' />");
     $("#menu").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href');
