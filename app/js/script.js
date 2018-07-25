c8d5db27130d687927d65ac27a8902a8996bb4cd
elena-vystavnaya <helenv2909@gmail.com>
2018-07-26 00:22:40 +0300

    change link css

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
-  //  $("head").append("<link rel='stylesheet' type='text/css' href='/css/style.min.css' />");
-    $("#menu").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href');
-        var top = $(id).offset().top;
-        var margin = $("#menu").height();
-        $('body,html').animate({
-            scrollTop: top - margin
-        }, 1000);
-    });
-});
+    //  $("head").append("<link rel='stylesheet' type='text/css' href='/css/style.min.css' />");
+      $("#menu").on("click","a", function (event) {
+          event.preventDefault();
+          var id  = $(this).attr('href');
+          var top = $(id).offset().top;
+          var margin = $("#menu").height();
+          $('body,html').animate({
+              scrollTop: top - margin
+          }, 1000);
+      });
+  }); 
+
 
