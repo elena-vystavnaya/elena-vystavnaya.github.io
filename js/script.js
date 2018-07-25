96f61258c037105dec064877f4ef52d31ca87852
elena-vystavnaya <helenv2909@gmail.com>
2018-07-25 23:19:20 +0300

    end all sections

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href');
+        var top = $(id).offset().top;
+        var margin = $("#menu").height();
+        $('body,html').animate({
+            scrollTop: top - margin
+        }, 1000);
+    });
+});
+
